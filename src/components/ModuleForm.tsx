a3d16f73b699236040deca6e5f91fcca2be523fb
redkorr <redkor910.97@gmail.com>
2024-07-09 15:22:06 +0200

    feat: created ModuleForm

---
+++ src/components/ModuleForm.tsx
@@ -0,0 +1,133 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z, ZodType } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useApi } from '@/hooks/useApi';
+import { useParams } from 'react-router-dom';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { useEffect, useState } from 'react';
+import { Module } from '@/types';
+import { Textarea } from './ui/textarea';
+import { DialogClose } from '@radix-ui/react-dialog';
+
+export const ModuleSchema: ZodType<Partial<Module>> = z.object({
+  name: z.optional(z.string()),
+  description: z.optional(z.string({ message: 'dupa' })),
+  targetTemperature: z.optional(
+    z.coerce
+      .number()
+      .gte(0, { message: 'The value needs to be greater or equal to 0' })
+      .lte(40, { message: 'The value needs to be smaller or equal to 40' })
+  ),
+});
+
+interface ModuleFormProps {
+  isSubmitted: boolean;
+  setIsSubmitted: (arg: boolean) => void;
+}
+
+const ModuleForm = ({ isSubmitted, setIsSubmitted }: ModuleFormProps) => {
+  const [formData, setFormData] = useState<Partial<Module>>({});
+
+  const { id } = useParams();
+  const { callApi } = useApi(
+    `/modules/${id}`,
+    { method: 'PATCH', data: formData },
+    false
+  );
+  const form = useForm({
+    resolver: zodResolver(ModuleSchema),
+  });
+
+  useEffect(() => {
+    if (isSubmitted) {
+      console.log('formDataChanged');
+      callApi();
+      setIsSubmitted(false);
+    }
+  }, [formData, isSubmitted]);
+
+  function removeEmptyFields(data: Partial<Module>) {
+    Object.keys(data).forEach((key) => {
+      if (
+        data[key as keyof typeof data] === '' ||
+        data[key as keyof typeof data] == null
+      ) {
+        delete data[key as keyof typeof data];
+      }
+    });
+  }
+
+  const onSubmit: SubmitHandler<Partial<Module>> = (data) => {
+    removeEmptyFields(data);
+    setFormData(data);
+    setIsSubmitted(true);
+  };
+  console.log(form.formState.touchedFields);
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='description'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='targetTemperature'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Target temperature</FormLabel>
+              <FormControl>
+                <Input
+                  type='number'
+                  {...field}
+                  onChange={(e) => {
+                    field.onChange(
+                      e.target.value === '' ? undefined : e.target.valueAsNumber
+                    );
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <DialogClose asChild>
+          <Button type='submit'>Submit</Button>
+        </DialogClose>
+      </form>
+    </Form>
+  );
+};
+
+export default ModuleForm;
--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,3 +1,5 @@
+import { ModeToggle } from '@/components/ModeToggle';
+import ModuleForm from '@/components/ModuleForm';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
 import {
@@ -7,23 +9,40 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { useApi } from '@/hooks/useApi';
 import { Module } from '@/types';
 import { AlertCircle, ArrowLeft, SquarePen } from 'lucide-react';
+import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const DetailsPage = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const { id } = useParams();
-  const { data, isLoading, error } = useApi<Module>(`/modules/${id}`);
+  const { data, isLoading, error, callApi } = useApi<Module>(`/modules/${id}`);
+
+  useEffect(() => {
+    callApi();
+  }, [isSubmitted]);
 
   return (
     <div className='flex flex-col gap-2 md:justify-center'>
-      <Button variant='outline' size='default' className=' max-w-24'>
-        <Link to='/' className='flex items-center justify-center gap-1'>
-          <ArrowLeft className='w-4' />
-          Go Back
-        </Link>
-      </Button>
+      <div className='flex md:max-w-[800px] justify-between'>
+        <Button variant='outline' size='default' className=' max-w-24'>
+          <Link to='/' className='flex items-center justify-center gap-1'>
+            <ArrowLeft className='w-4' />
+            Go Back
+          </Link>
+        </Button>
+        <ModeToggle />
+      </div>
       {isLoading && <p>Loading...</p>}
       <div className='flex flex-col gap-2 md:max-w-[800px]'>
         {!data?.available && (
@@ -47,8 +66,7 @@ const DetailsPage = () => {
             <CardContent>
               <div className='flex justify-around text-sm md:justify-center md:gap-8 text-muted-foreground'>
                 <div
-                  id='availability'
-                  className={`rounded-md px-3 py-2 font-medium md:w-40 ${
+                  className={`flex items-center rounded-md px-3 py-2 font-medium md:w-40 justify-center ${
                     data.available
                       ? 'bg-green-500/10 text-green-500'
                       : 'bg-red-500/10 text-red-500'
@@ -57,13 +75,30 @@ const DetailsPage = () => {
                   {data.available ? 'Available' : 'Unavailable'}
                 </div>
                 <div className='flex items-center rounded-md bg-secondary text-secondary-foreground'>
-                  <Button
-                    disabled={!data.available}
-                    className='flex gap-1 md:w-40'
-                  >
-                    <SquarePen className='w-4 h-4' />
-                    <span>Edit</span>
-                  </Button>
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Button
+                        disabled={!data.available}
+                        className='flex gap-1 md:w-40'
+                      >
+                        <SquarePen className='w-4 h-4' />
+                        <span>Edit</span>
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent className='sm:max-w-[425px]'>
+                      <DialogHeader>
+                        <DialogTitle>Edit profile</DialogTitle>
+                        <DialogDescription>
+                          Make changes to module here. Click save when you're
+                          done.
+                        </DialogDescription>
+                      </DialogHeader>
+                      <ModuleForm
+                        isSubmitted={isSubmitted}
+                        setIsSubmitted={setIsSubmitted}
+                      />
+                    </DialogContent>
+                  </Dialog>
                 </div>
               </div>
             </CardContent>
