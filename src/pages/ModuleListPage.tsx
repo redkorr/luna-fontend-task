1b009b32e23ddfd77afe3e7bf7e767bd55911a5f
redkorr <redkor910.97@gmail.com>
2024-07-10 22:51:25 +0200

    feat: updated readme

--- src/pages/ModuleListPage.tsx
+++ src/pages/ModuleListPage.tsx
@@ -1,8 +1,11 @@
 import Navigation from '@/components/Navigation';
+import Spinner from '@/components/Spinner';
+import { Alert, AlertTitle } from '@/components/ui/alert';
 import { columns } from '@/components/ui/columns';
 import { DataTable } from '@/components/ui/data-table';
 import { useApi } from '@/hooks/useApi';
 import { Module } from '@/types';
+import { AlertCircle } from 'lucide-react';
 
 const ModuleListPage = () => {
   const { data, isLoading, error } = useApi<Array<Module>>('/modules');
@@ -11,10 +14,15 @@ const ModuleListPage = () => {
     <div className='flex flex-col items-center gap-2'>
       <Navigation />
 
-      {isLoading && <p>Loading...</p>}
+      {isLoading && <Spinner />}
       {data && <DataTable columns={columns} data={data} />}
 
-      {error && <p>{error.message}</p>}
+      {error && (
+        <Alert variant='destructive'>
+          <AlertCircle className='w-6 h-6' />
+          <AlertTitle>{error.message}</AlertTitle>
+        </Alert>
+      )}
     </div>
   );
 };
